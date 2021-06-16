877a71e1937282c7ce91935cfbfb1ee57ff488db
Ilya Makarov <ilya.makarov.592@gmail.com>
2021-06-16 21:53:44 +0300

    bb print

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -1,14 +1,24 @@
 #include "bitboard.h"
 #include "misc.h"
 
+constexpr const char *RANK_STR = "12345678";
+constexpr const char *FILE_STR = "abcdefgh";
+
 void print_bb(Bitboard bb)
 {
-    for (Rank rank = RANK_8; rank >= RANK_1; --rank) {
-        PUTC('|');
-        for (File file = FILE_A; file <= FILE_H; ++file) {
-            PUTC(get(bb, square(rank, file)) ? 'X' : '-');
-            PUTC('|');
+    PUTC('\n');
+    for (Rank r = RANK_8; r >= RANK_1; --r) {
+        PRINT("%c   ", RANK_STR[r]);
+        for (File f = FILE_A; f <= FILE_H; ++f) {
+            PUTC(get(bb, square(r, f)) ? 'X' : '-');
+            PUTC(' ');
         }
         PUTC('\n');
     }
+    PRINT("\n   ");
+    for (File f = FILE_A; f <= FILE_H; ++f)
+        PRINT(" %c", FILE_STR[f]);
+    PRINT("\n\n");
+    PRINT("Hex: %016lX\n", bb);
+    PUTC('\n');
 }
