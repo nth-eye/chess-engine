dce22b0a8a4ed2927e1cb1dbcdb07a51f3167f3e
Ilya Makarov <ilya.makarov.592@gmail.com>
2021-06-16 22:13:05 +0300

    replaced lookup-table with shift

--- inc/misc.h
+++ inc/misc.h
@@ -2,7 +2,7 @@
 #define MISC_H
 
 #include <cstdio>
-#include <array>
+// #include <array>
 
 #define PRINT   printf
 #define PUTC    putchar
@@ -20,22 +20,13 @@ constexpr Rank& operator--(Rank &d)         { return d = Rank(int(d) - 1); }
 constexpr Square& operator++(Square &d)     { return d = Square(int(d) + 1); }
 constexpr Square& operator--(Square &d)     { return d = Square(int(d) - 1); }
 
-constexpr auto generate_sq_bb()
-{
-    std::array<Bitboard, SQ_num> sq_bb = {};
-    for (Square sq = A1; sq <= H8; ++sq)
-        sq_bb[sq] = Bitboard(1ULL << sq);
-    return sq_bb;
-}
-constexpr auto SQ_BB = generate_sq_bb();
-
 constexpr File file(Square s)               { return File(s & 7); }
 constexpr Rank rank(Square s)               { return Rank(s >> 3); }
 constexpr Square square(Rank r, File f)     { return Square((r << 3) + f); }
-constexpr Bitboard bitboard(Square sq)      { return SQ_BB[sq]; }
+constexpr Bitboard bit(Square sq)           { return 1 << sq; }
 
-constexpr void set(Bitboard &bb, Square sq) { bb |= bitboard(sq); }
-constexpr void clr(Bitboard &bb, Square sq) { bb &= ~bitboard(sq); }
-constexpr bool get(Bitboard bb, Square sq)  { return bb & bitboard(sq); }
+constexpr void set(Bitboard &bb, Square sq) { bb |= bit(sq); }
+constexpr void clr(Bitboard &bb, Square sq) { bb &= ~bit(sq); }
+constexpr bool get(Bitboard bb, Square sq)  { return bb & bit(sq); }
 
 #endif // MISC_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <cstdio>
+#include <ctime>
 #include "bitboard.h"
+#include "misc.h"
+
+// Measure execution time of a function.
+template<size_t N = 1, bool Avg = true, class Fn, class ...Args>
+clock_t measure_time(Fn &&fn, Args &&...args)
+{
+    clock_t begin = clock();
+    for (size_t i = 0; i < N; ++i) 
+        fn(args...);
+    clock_t end = clock();
+    clock_t res = end - begin;
+    return Avg ? res / N : res;
+}
 
 int main(int, char**) 
 {
     Bitboard bb = 0xff00ff00ff00ff00;
+
+    printf("Shift:  %3ld clock_t\n", 
+        measure_time<100000, false>(bit, H8));
 
     print_bb(bb);
 }
